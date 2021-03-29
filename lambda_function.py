705c5587d88d3af2888ae89d13b801dc9bac89d8
developer <alain.rodriguez@woombatcg.com>
2021-03-29 15:11:26 -0500

    test

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
+    df = pd.DataFram(data=d)
     print(df)
     print('Hecho')
     print('Prueba Realizada con exito de DevOps con Lambda y Pipeline')
